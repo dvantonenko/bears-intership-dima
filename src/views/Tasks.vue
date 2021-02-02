21ef714e83669ab6f5d7ecd0f8e9bb6baa14cc1d
Dima <dantonenko5482@gmail.com>
2021-02-02 16:51:23 +0200

    add trello tasts

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -7,7 +7,9 @@
         class="input_add_column"
         v-model="newColumn"
       />
-      <button class="add_column_button" @click="addNewColumn">Add column</button>
+      <button class="add_column_button add_button_style" @click="addNewColumn">
+        Add column
+      </button>
     </div>
 
     <draggable
@@ -23,37 +25,68 @@
           @click="deleteColumn"
           :id="column.id"
         />
-        <div style="padding: 2px">
-          <!-- <input
-            @keyup.enter="addNewTask"
-            placeholder="Enter Task"
-            class="input_add_task"
-            v-model="arrColumns[column.id - 1].text"
-            :id="column.id"
-            @click="addNewTask"
-          /> -->
-          <h2>{{ column.name }}</h2>
 
-          <draggable
-            class="drag_n_drop_task"
-            :list="column.tasks"
-            group="tasks"
-            :options="{ animation: 150 }"
-          >
-            <div class="task_elem" v-for="element in column.tasks" :key="element.name">
-              {{ element.name }}
-              <button class="trash_icon">
+        <input
+          @keyup.enter="addNewTask"
+          placeholder="Enter Task"
+          class="input_add_task"
+          v-model="arrColumns.find((item) => item.id === column.id).text"
+          :id="column.id"
+          @click="addNewTask"
+        />
+        <h2>
+          {{ column.name }}
+        </h2>
+
+        <draggable
+          class="drag_n_drop_task"
+          :list="column.tasks"
+          group="tasks"
+          :options="{ animation: 150 }"
+        >
+          <div class="task_elem" v-for="element in column.tasks" :key="element.name">
+            {{ element.name }}
+            <div v-if="showUpdateInput" class="update_modal">
+              <div class="update_block">
                 <img
-                  src="../assets/icons/trash-2.svg"
-                  v-on:click="deleteCard"
-                  :name="element.name"
-                  :id="column.id"
+                  src="../assets/icons/cancel.svg"
+                  class="close_update"
+                  @click="closeModalUpdate"
                 />
-              </button>
-              <div v-if="!column.tasks.length" style="height: 300px"></div>
+                <label class="label_input" for="update"><h2>Update Card</h2></label>
+                <div style="height: 70px">
+                  <input
+                    class="update_input"
+                    v-model="toUpdateText"
+                    type="text"
+                    id="update"
+                  />
+                </div>
+
+                <button class="save_button add_button_style" @click="saveUpdate">
+                  Save
+                </button>
+              </div>
             </div>
-          </draggable>
-        </div>
+            <img
+              class="update_icon"
+              src="../assets/icons/refresh-ccw.svg"
+              @click="updateCard"
+              :name="element.name"
+              :id="column.id"
+            />
+            <button class="trash_icon">
+              <img
+                src="../assets/icons/trash-2.svg"
+                v-on:click="deleteCard"
+                :name="element.name"
+                :id="column.id"
+              />
+            </button>
+
+            <div v-if="!column.tasks.length" style="height: 300px"></div>
+          </div>
+        </draggable>
       </div>
     </draggable>
   </div>
@@ -66,10 +99,11 @@ export default {
   },
   data() {
     return {
+      colorRed: "red",
       newTask: "",
       newColumn: "",
       arrColumns: [
-        { name: "Backlog", tasks: [{ name: "Code SignPage" }], id: 1 },
+        { name: "Backlog", tasks: [{ name: "Code SignPage" }], id: 1, text: "" },
         {
           name: "In Process",
           tasks: [{ name: "Test DashBoard" }],
@@ -89,14 +123,20 @@ export default {
           text: "",
         },
       ],
+      showUpdateInput: false,
+      toUpdateText: "",
+      toSaveText: "",
+      indexUpdate: null,
     };
   },
   methods: {
     addNewTask(e) {
-      console.log();
-      const text = this.arrColumns[e.target.id - 1].text;
-      if (text) {
-        this.arrColumns[e.target.id - 1].tasks.push({ name: text });
+      const index = this.arrColumns.findIndex((item) => item.id === Number(e.target.id));
+      let createTask = this.arrColumns[index];
+      if (createTask.text) {
+        createTask.tasks.push({ name: createTask.text });
+        createTask.text = "";
+        this.arrColumns[index] = createTask;
       }
     },
     addNewColumn() {
@@ -107,7 +147,6 @@ export default {
           id: this.arrColumns.length + 1,
           text: "",
         });
-
         this.newColumn = "";
       }
     },
@@ -118,12 +157,36 @@ export default {
       }
     },
     deleteCard(e) {
-      const index = this.arrColumns[e.target.id - 1].tasks.findIndex(
-        (n) => n.name === e.target.name
-      );
-      if (index !== -1) {
-        this.arrColumns[e.target.id - 1].tasks.splice(index, 1);
-      }
+      const findColumnTasks = this.arrColumns
+        .find((item) => item.id === Number(e.target.id))
+        .tasks.filter((item) => item.name != e.target.name);
+      const index = this.arrColumns.findIndex((item) => item.id === Number(e.target.id));
+      this.arrColumns[index].tasks = findColumnTasks;
+    },
+    updateCard(e) {
+      this.toUpdateText = e.target.name;
+      this.toSaveText = e.target.name;
+      this.indexUpdate = e.target.id;
+      this.showUpdateInput = true;
+    },
+    closeModalUpdate() {
+      this.showUpdateInput = false;
+      this.clearProps();
+    },
+    saveUpdate() {
+      const res = this.arrColumns.find((item) => item.id === Number(this.indexUpdate));
+      res.tasks.forEach((item) => {
+        if (item.name === this.toSaveText && this.toUpdateText.length !== 0) {
+          item.name = this.toUpdateText;
+          this.clearProps();
+        }
+      });
+    },
+    clearProps() {
+      this.indexUpdate = null;
+      this.toUpdateText = "";
+      this.toSaveText = "";
+      this.showUpdateInput = false;
     },
   },
   watch: {
@@ -134,31 +197,47 @@ export default {
 };
 </script>
 <style scoped>
+.pointer-events {
+  color: red;
+}
 .header {
   display: flex;
+  width: 20%;
+  margin: 0 auto;
 }
 .input_add_column {
-  border: 1px solid black;
+  border: 2px solid transparent;
+  border-bottom: 2px solid grey;
   margin: 10px;
   padding: 10px;
   height: 20px;
   outline: none;
   float: left;
+  color: black;
+  font-size: 16px;
 }
 .input_add_task {
-  border: 1px solid black;
+  border: 2px solid transparent;
+  border-bottom: 2px solid grey;
   margin: 10px;
   padding: 10px;
   height: 20px;
   outline: none;
+  background: none;
+  color: black;
+}
+.input_add_task:focus {
+  border: 2px solid grey;
+  background: white;
 }
 .add_column_button {
+  width: 80px;
   height: 42px;
-  width: 60px;
-  margin: 10px;
-  border-radius: 10px;
-  border: none;
-  background-color: rgb(52, 52, 189);
+  font-size: 16px;
+  margin: 10px 0;
+}
+.add_column_button:hover {
+  box-shadow: 0px 0px 13px -1px #000000;
 }
 .add_task_button {
   height: 42px;
@@ -177,12 +256,14 @@ export default {
   position: relative;
   float: left;
   width: 20%;
+  min-width: 280px;
   border: 1px solid grey;
   border-radius: 20px;
   min-height: 300px;
   margin: 5px;
-  background-color: #00408538;
+  background-color: #a9ee29e3;
 }
+
 .task_elem {
   position: relative;
   height: 40px;
@@ -190,13 +271,6 @@ export default {
   background-color: white;
   margin: 0 10px;
   line-height: 40px;
-}
-
-.flip-list-move {
-  transition: transform 0.5s;
-}
-.no-move {
-  transition: transform 0s;
 }
 .drag_n_drop_task {
   min-height: 300px;
@@ -207,6 +281,7 @@ export default {
   position: absolute;
   top: 20px;
   right: 20px;
+  cursor: pointer;
 }
 .trash_icon {
   position: absolute;
@@ -215,6 +290,73 @@ export default {
   background-color: white;
   border: none;
   outline: none;
+  cursor: pointer;
+}
+.update_icon {
+  position: absolute;
+  top: 10px;
+  left: 20px;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+.update_modal {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+.update_block {
+  position: relative;
+  width: 500px;
+  height: 200px;
+  background-color: white;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: none;
+  border-radius: 20px;
+}
+.update_input {
+  margin: 20px auto;
+  border: none;
+  border-bottom: 4px solid rgba(0, 194, 178, 0.733);
+  padding: 10px;
+  color: black;
+  font-size: 16px;
+  transform: translateY(-50%);
+  text-align: center;
+  height: 40px;
+  width: 30%;
+  box-sizing: border-box;
+  outline: none;
+  transition: 0.5s;
+}
+.update_input:focus {
+  border-bottom: 4px solid rgba(0, 194, 16, 0.733);
+  width: 70%;
+}
+.save_button {
+  width: 80px;
+  height: 40px;
+}
+
+.save_button:hover {
+  box-shadow: 0px 0px 13px -1px #000000;
+}
+.label_input {
+  line-height: 50px;
+  cursor: pointer;
+}
+.close_update {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 20px;
+  height: 20px;
   cursor: pointer;
 }
 </style>
